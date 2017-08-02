5247bcf430a9ae9e75830c7d806a32a616a10201
tyronfong <tyronfong@outlook.com>
2017-08-03 03:03:46 +0800

    Django learning

--- polls/models.py
+++ polls/models.py
@@ -25,3 +25,10 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+class Word(models.Model):
+    word = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.word
